94efa983972a6b88e9b84f0ffe3ab12cec6ecfc7
Anaïs Siba <anais.siba@ankorstore.com>
2023-03-23 17:27:32 +0100

    Init nuxt blog app

---
+++ layouts/posts.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header>HELLO</header>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Merriweather", serif;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="posts-page"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: 1,
+    };
+  },
+  methods: {
+    onLoadPost() {
+      this.$router.push("/posts/" + this.id);
+    },
+  },
+  layout: "posts",
+};
+</script>
